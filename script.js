22a291be3adb66b5be21969af04c7c6778c0ce11
Daniel-pixel-cpu <danieldutradeoliveira10@gmail.com>
2025-07-28 10:09:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -23,3 +23,10 @@ botaoTema.addEventListener("click", () => {
 voltarTopo.addEventListener("click", () => {
   window.scrollTo({ top: 0, behavior: "smooth" });
 });
+
+document.querySelectorAll('.clicavel').forEach(section => {
+  section.addEventListener('click', () => {
+    const link = section.getAttribute('data-link');
+    if (link) window.location.href = link;
+  });
+});
